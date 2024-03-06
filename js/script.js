018a16dbfaf588e6c16d3b6daf915381635443f7
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-03-06 16:11:35 +0100

    added dynamic page navigation

--- js/script.js
+++ js/script.js
@@ -1,29 +1,75 @@
 const container = document.getElementById("container");
-
+const prevBTN = document.getElementById("prevPage");
+const nextBTN = document.getElementById("nextPage");
+let pageNum = 1;
 let products = [];
 
-fetch("https://kassal.app/api/v1/products", {
-    method: "Get",
+fetch("https://kassal.app/api/v1/products?page=1", {
+    method: "GET",
     headers: {
         Authorization: "Bearer YTzc29dgYRKS0pMRtLeakhxiVSwUZe56i9ki9OCm",
     },
 })
-    .then((resp) => {
-        return resp.json();
-    })
+    .then((resp) => resp.json())
     .then((productResult) => {
         products = productResult;
-
-        for (const prod of products) {
-            display(...prod);
-        }
+        products.data.map((product) => {
+            displayProduct(product);
+        });
+    })
+    .catch((error) => {
+        console.error("Error fetching product data:", error);
     });
 
-function displayProducts(prod) {
+function displayProduct(data) {
     const prodDiv = document.createElement("div");
     const prodName = document.createElement("h2");
-    prodName.innerText = prod.name;
+    prodName.innerText = data.name;
     const prodImg = document.createElement("img");
+    prodImg.src = data.image;
     prodDiv.appendChild(prodName);
     prodDiv.appendChild(prodImg);
+    container.appendChild(prodDiv);
 }
+
+nextBTN.addEventListener("click", function () {
+    container.innerHTML = "";
+    fetch(`https://kassal.app/api/v1/products?page=${pageNum}`, {
+        method: "GET",
+        headers: {
+            Authorization: "Bearer YTzc29dgYRKS0pMRtLeakhxiVSwUZe56i9ki9OCm",
+        },
+    })
+        .then((resp) => resp.json())
+        .then((productResult) => {
+            products = productResult;
+            products.data.map((product) => {
+                displayProduct(product);
+                pageNum++;
+            });
+        })
+        .catch((error) => {
+            console.error("Error fetching product data:", error);
+        });
+});
+
+prevBTN.addEventListener("click", function () {
+    container.innerHTML = "";
+    fetch(`https://kassal.app/api/v1/products?page=${pageNum}`, {
+        method: "GET",
+        headers: {
+            Authorization: "Bearer YTzc29dgYRKS0pMRtLeakhxiVSwUZe56i9ki9OCm",
+        },
+    })
+        .then((resp) => resp.json())
+        .then((productResult) => {
+            products = productResult;
+            products.data.map((product) => {
+                displayProduct(product);
+                pageNum--;
+            });
+        })
+        .catch((error) => {
+            console.error("Error fetching product data:", error);
+        });
+});
